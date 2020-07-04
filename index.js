9d1432efac8bca0f2ef3343a3ab002af17e5d3b0
Mike Marien <mikemarien@protonmail.ch>
2020-07-03 22:08:12 -0500

    added questions to index.js

--- index.js
+++ index.js
@@ -1,9 +1,57 @@
-
-
+// require statement to import inquirer package.
 var inquirer = require("inquirer");
 
+//inquirer .prompt method to prompt user with a series of questions
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "Title:",
+      name: "title"
+    },
+    {
+      type: "input",
+      message: "Description:",
+      name: "description"
+    },
+    {
+      type: "input",
+      message: "Table of Contents:",
+      name: "tableOfContents"
+    },
+    {
+      type: "input",
+      message: "Installation:",
+      name: "installation"
+    },
+    {
+      type: "input",
+      message: "Usage:",
+      name: "usage"
+    },
+    {
+      type: "input",
+      message: "License:",
+      name: "title"
+    },
+    {
+      type: "input",
+      message: "Contributing:",
+      name: "contributing"
+    },
+    {
+      type: "input",
+      message: "Tests:",
+      name: "tests"
+    },
+    {
+      type: "input",
+      message: "Questions:",
+      name: "questions"
+    },
 
 
+  ])
 
 
 
